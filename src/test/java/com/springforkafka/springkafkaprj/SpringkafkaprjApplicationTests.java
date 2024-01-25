5759e5ac0e5f9e703f8ce2bdd24595b7a0ef5557
DESKTOP-F12VINP\User <yush@obzen.com>
2024-01-25 10:53:30 +0900

    Initial commit

---
+++ src/test/java/com/springforkafka/springkafkaprj/SpringkafkaprjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springforkafka.springkafkaprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringkafkaprjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
